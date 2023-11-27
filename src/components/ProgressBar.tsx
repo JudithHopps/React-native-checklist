022aa971eacdc62a3e0ea3716ac60b326620d9ef
JudithHopps <kookjeayon100@gmail.com>
2023-11-27 21:06:53 +0900

    feat : weeklyData

---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+
+type ProgressBarProps = {
+  completedCount: number;
+  totalItems: number;
+};
+
+const ProgressBar: React.FC<ProgressBarProps> = ({
+  completedCount,
+  totalItems,
+}) => {
+  const percentage = totalItems > 0 ? (completedCount / totalItems) * 100 : 0;
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.progressDetail}>
+        <Text style={styles.completedTextStyle}>
+          {completedCount} of {completedCount} completed{' '}
+        </Text>
+        <Text style={styles.percentageTextStyle}>{percentage}%</Text>
+      </View>
+
+      <View style={styles.progressBar}>
+        <View style={{...styles.progressIndicator, width: `${percentage}%`}} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: 335,
+    marginBottom: 28,
+  },
+  progressDetail: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  completedTextStyle: {
+    color: '#333',
+    fontSize: 16,
+    fontWeight: '700',
+  },
+  percentageTextStyle: {
+    color: '#0BB',
+    fontSize: 14,
+    fontWeight: '700',
+    paddingTop: 2,
+  },
+  progressBar: {
+    backgroundColor: '#ddd',
+    marginTop: 17,
+  },
+  progressIndicator: {
+    height: 6,
+    backgroundColor: '#44CEC6',
+    borderRadius: 5,
+  },
+});
+
+export default ProgressBar;
--- src/components/WeeklyData.tsx
+++ src/components/WeeklyData.tsx
@@ -1,32 +1,99 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
+import {checkboxIcon} from '../assets/svgs/svg';
+import ProgressBar from './ProgressBar';
 
-const styles = StyleSheet.create({
-  container: {
-    width: 375,
-    height: 60,
-    flexShrink: 0,
-    paddingVertical: 20,
-    paddingHorizontal: 146,
-    paddingBottom: 18,
-  },
-  week: {
-    fontSize: 16,
-    fontWeight: '700',
-    color: '#333',
-  },
-});
-
-type WeeklyDataProps = {
-  weekData: string;
+type ChecklistFetchItem = {
+  weekNumber: number;
+  content: string;
 };
 
-const WeeklyData: React.FC<WeeklyDataProps> = ({weekData}) => {
+type ChecklistItem = {
+  weekNumber: number;
+  content: string;
+  completed: boolean;
+};
+
+type WeeklyDataProps = {
+  selectedWeek: number;
+};
+
+const WeeklyData: React.FC<WeeklyDataProps> = ({selectedWeek}) => {
+  const [checklistData, setChecklistData] = useState<ChecklistItem[]>([]);
+  const [selectedWeekData, setSelectedWeekData] = useState<ChecklistItem[]>([]);
+  const [completedCount, setCompletedCount] = useState<number>(0);
+  const [totalItems, setTotalItems] = useState<number>(0);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          'https://file.notion.so/f/f/772fc649-1fcc-498c-94cc-cff14dc51887/cd375ffb-16ed-4926-bda3-d0b9605addf0/checklist_seeds.json?id=6dd919ce-57bd-4ca3-91f0-2923b9433edd&table=block&spaceId=772fc649-1fcc-498c-94cc-cff14dc51887&expirationTimestamp=1701165600000&signature=w7_V9XeCMfTwcb_621a3Bdkk4twojcAJsrGtdi88HAA&downloadName=checklist_seeds.json',
+        );
+        const data: ChecklistFetchItem[] = await response.json();
+
+        const initializedData: ChecklistItem[] = data.map(item => ({
+          ...item,
+          completed: false,
+        }));
+        setChecklistData(initializedData);
+        // console.log(initializedData);
+      } catch (error) {
+        console.error('Error fetching data', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const getFilteredData = () => {
+      return checklistData.filter(item => item.weekNumber === selectedWeek);
+    };
+
+    const filteredData = getFilteredData();
+    const completedTasks = filteredData.filter(item => item.completed);
+
+    setSelectedWeekData(filteredData);
+    setCompletedCount(completedTasks.length);
+    setTotalItems(filteredData.length);
+  }, [checklistData, selectedWeek]);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.week}>{weekData}</Text>
+      <ProgressBar totalItems={totalItems} completedCount={completedCount} />
+      <FlatList
+        data={selectedWeekData}
+        renderItem={({item}) => (
+          <View style={styles.checklistContatiner}>
+            {checkboxIcon(item.completed)}
+            <Text style={styles.week}>{item.content}</Text>
+          </View>
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flexShrink: 0,
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    paddingBottom: 18,
+  },
+  checklistContatiner: {
+    display: 'flex',
+    flexDirection: 'row',
+    paddingVertical: 10,
+  },
+  week: {
+    fontSize: 14,
+    fontWeight: '400',
+    color: '#333',
+    paddingLeft: 12,
+  },
+});
 
 export default WeeklyData;
